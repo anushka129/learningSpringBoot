9508d28fe34612be3842dcab9e29656813e50fa2
Anushka Agarwal <anushkaagarwal1203@gmail.com>
2026-02-02 14:55:21 +0530

    Request Validation in Spring MVC

--- src/main/java/com/example/learningSpringBootApp/controller/StudentController.java
+++ src/main/java/com/example/learningSpringBootApp/controller/StudentController.java
@@ -3,6 +3,7 @@ package com.example.learningSpringBootApp.controller;
 import com.example.learningSpringBootApp.dto.AddStudentRequestDto;
 import com.example.learningSpringBootApp.dto.StudentDto;
 import com.example.learningSpringBootApp.service.StudentService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -31,7 +32,7 @@ public class StudentController {
     }
 
     @PostMapping
-    public ResponseEntity<StudentDto> createNewStudent(@RequestBody AddStudentRequestDto addStudentRequestDto){
+    public ResponseEntity<StudentDto> createNewStudent(@RequestBody @Valid AddStudentRequestDto addStudentRequestDto){
         return ResponseEntity.status(HttpStatus.CREATED).body(studentService.createNewStudent(addStudentRequestDto));
     }
 
